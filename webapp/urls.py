be1fbc8a4a22d8620a5c3b4fea8510a53883e46a
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-25 16:46:50 +0600

    reformat urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,18 +1,37 @@
-from django.urls import path
+from django.urls import path, include
 
-from webapp.views.ad_views import AdsListView, AdCreateView, AdDetailView, AdUpdateView, AdDeleteView
-from webapp.views.to_moderate_ad_views import AdsToModerateListView, AdToModerateDetailView, AdPublishView, AdRejectView
+from webapp.views.ad_views import (
+    AdsListView,
+    AdCreateView,
+    AdDetailView,
+    AdUpdateView,
+    AdDeleteView,
+)
+from webapp.views.to_moderate_ad_views import (
+    AdsToModerateListView,
+    AdToModerateDetailView,
+    AdPublishView,
+    AdRejectView,
+)
 
 app_name = 'webapp'
 
+ads_urlpatterns = [
+    path('create/', AdCreateView.as_view(), name='ads_create'),
+    path('<int:pk>/', AdDetailView.as_view(), name='ads_detail'),
+    path('<int:pk>/update', AdUpdateView.as_view(), name='ads_update'),
+    path('<int:pk>/delete/', AdDeleteView.as_view(), name='ads_delete'),
+]
+
+moderate_ads_urlpatterns =[
+    path('', AdsToModerateListView.as_view(), name='ads_to_moderate_liist'),
+    path('<int:pk>/', AdToModerateDetailView.as_view(), name='ads_to_moderate_detail'),
+    path('<int:pk>/publish', AdPublishView.as_view(), name="ads_to_moderate_publish"),
+    path('<int:pk>/reject', AdRejectView.as_view(), name="ads_to_moderate_reject"),
+]
+
 urlpatterns = [
     path('', AdsListView.as_view(), name='index'),
-    path('moderate_ads/', AdsToModerateListView.as_view(), name='ads_to_moderate_liist'),
-    path('moderate_ads/<int:pk>/', AdToModerateDetailView.as_view(), name='ads_to_moderate_detail'),
-    path('moderate_ads/<int:pk>/publish', AdPublishView.as_view(), name="ads_to_moderate_publish"),
-    path('moderate_ads/<int:pk>/reject', AdRejectView.as_view(), name="ads_to_moderate_reject"),
-    path('ad/create/', AdCreateView.as_view(), name='ads_create'),
-    path('ad/<int:pk>/', AdDetailView.as_view(), name='ads_detail'),
-    path('ad/<int:pk>/update', AdUpdateView.as_view(), name='ads_update'),
-    path('ad/<int:pk>/delete/', AdDeleteView.as_view(), name='ads_delete'),
+    path('ads/', include(ads_urlpatterns)),
+    path('moderate_ads/', include(moderate_ads_urlpatterns)),
 ]
